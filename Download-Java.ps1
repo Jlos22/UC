db6c3d30b04975bdc420f90a0c7bbc803542e3ab
Jlos22 <92428651+Jlos22@users.noreply.github.com>
2021-10-13 09:06:47 +0100

    Add files via upload

---
+++ Download-Java.ps1
@@ -0,0 +1,26 @@
+
+#Check Path
+$path = "C:\UC-One"
+if(!(Test-Path -Path $path))
+ {
+    New-Item -Path 'C:\UC-One' -ItemType Directory
+  }
+else
+  {
+   Write-host "Files already Created" -ForegroundColor Red
+  }
+
+#Gathers Files required
+Invoke-WebRequest -Uri https://cdnstoragepublic.blob.core.windows.net/cdndelivery/UC-One.msi -outfile "C:\UC-One\UC-One.msi"
+
+#wait for download
+Start-Sleep -Seconds 120
+
+Set-Location C:\UC-One\
+
+.\UC-One.msi /quiet /passive
+
+
+
+
+
