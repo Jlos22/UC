1d30e7851d0dbe4d414b97f2f7d10d1dda0103bb
Jlos22 <92428651+Jlos22@users.noreply.github.com>
2021-10-13 09:48:39 +0100

    Update Download-Java.ps1

--- Download-Java.ps1
+++ Download-Java.ps1
@@ -4,17 +4,14 @@ $path = "C:\UC-One"
 if(!(Test-Path -Path $path))
  {
     New-Item -Path 'C:\UC-One' -ItemType Directory
+    #Gathers Files required
+    ###Invoke-WebRequest -Uri https://cdnstoragepublic.blob.core.windows.net/cdndelivery/UC-One.msi -outfile "C:\UC-One\UC-One.msi" -UseBasicParsing
+    Start-BitsTransfer -Source "https://cdnstoragepublic.blob.core.windows.net/cdndelivery/UC-One.msi" -Destination "C:\UC-One\UC-One.msi"
   }
 else
   {
    Write-host "Files already Created" -ForegroundColor Red
   }
-
-
-
-#Gathers Files required
-###Invoke-WebRequest -Uri https://cdnstoragepublic.blob.core.windows.net/cdndelivery/UC-One.msi -outfile "C:\UC-One\UC-One.msi" -UseBasicParsing
-Start-BitsTransfer -Source "https://cdnstoragepublic.blob.core.windows.net/cdndelivery/UC-One.msi" -Destination "C:\UC-One\UC-One.msi"
 
 
 Set-Location C:\UC-One\
